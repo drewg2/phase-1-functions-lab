74ed158f8f42049878b8162c95ef1e6e539ffb21
Drew Grey <dtsgrey@gmail.com>
2022-02-01 10:35:43 -0600

    commit tree 034a1e5f82abf74560b0bb2921ddee063cd8d4d6

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(block1) {
+    let distance;
+    if(block1 > 42) {
+        return distance = block1 - 42;
+    }
+    else if (block1 < 42) {
+        return distance = 42 - block1;
+    }
+}
+
+function distanceFromHqInFeet(block2) {
+     return distanceFromHqInBlocks(block2) * 264;
+}
+
+function distanceTravelledInFeet(start,destination) {
+    let feetTravelled;
+    if(start>destination) {
+       return feetTravelled = (start - destination) * 264;
+    }
+    else {
+       return feetTravelled = (destination - start) *264;
+    }
+}
+
+function calculatesFarePrice(start,destination) {
+    let price;
+    
+    if (distanceTravelledInFeet(start,destination) < 400) {
+        return 0;
+    }
+    
+    else if (distanceTravelledInFeet(start,destination) > 400 && distanceTravelledInFeet(start,destination) < 2000) {
+        return price = (distanceTravelledInFeet(start,destination) - 400) * .02;
+    }
+    
+    else if (distanceTravelledInFeet(start,destination) > 2000 && distanceTravelledInFeet(start,destination) < 2500) {
+        return 25;
+    }
+
+    else {
+        return "cannot travel that far";
+    }
+
+}
